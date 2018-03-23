724cbefc374c912ccbe11869ca46e6cc931e0042
Gustao <gustavorag@gmail.com>
2018-03-22 19:29:56 -0700

    Initial structure from bootstrap template

---
+++ js/quizzes.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	// Quizz content should follow this format:
+	/* { quizzName: "name",
+		 quizzTime: timeInSeconds;
+		 questions: [
+		 	{question: "enunciate",
+		 	options: [{
+				key: "A",
+				value: "Answer"
+		 	}],
+		 	studentAnswer: undefined,
+		 	rightQuestionKey: "A"}
+		 ]}
+	*/ 
+	function createQuizz(targetId, quizzContent){
+
+	}
+})
